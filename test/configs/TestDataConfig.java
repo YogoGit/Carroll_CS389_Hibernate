bc536ccdf43d0b048a807952961117e507d56254
Nate Williams <nate@yogotech.com>
2017-03-03 09:19:47 -0700

    Revert "Add debugging so we can see the SQL output"
    This reverts commit 5c1db21a2df8a1f2f657d367c8ca4041284cf39a.

--- test/configs/TestDataConfig.java
+++ test/configs/TestDataConfig.java
@@ -25,7 +25,7 @@ public class TestDataConfig {
     @Bean
     public EntityManagerFactory entityManagerFactory() {
         HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setShowSql(true);
+        vendorAdapter.setShowSql(false);
         vendorAdapter.setGenerateDdl(true);
         vendorAdapter.setDatabase(Database.H2);
 
