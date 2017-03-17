e16b954020fb8564b0782ec557a40a7000f0d85e
Nate Williams <nate@yogotech.com>
2017-03-16 21:53:31 -0600

    Add Dockerfile and build script

---
+++ docker/docker.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -x
+
+TEMP=$(mktemp -d)
+
+ZIP=play2torial-1.0-SNAPSHOT.zip
+
+cp ../target/universal/$ZIP $TEMP
+cp Dockerfile $TEMP
+cd $TEMP
+docker build -t playapp .
+
+cd /
+rm -rf $TEMP
